30ec52c7aaaf0b707e91cbdf175394fc45e09a48
Angel hernandez <darkm5584@gmail.com>
2026-06-15 04:18:12 -0600

    fix: manejo de excepciones y corrección de validación en app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
 from clima import get_weather
 
+load_dotenv()
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,10 +13,10 @@ def index():
 def weather():
     city = request.form['city']
     weather_data = get_weather(city) 
-    if weather_data.get:
+    if 'error' not in weather_data:
         return render_template('weather.html', weather=weather_data,city=city)
     else:
-        return render_template('index.html', error="No se encontró información del clima para la ciudad especificada.")
+        return render_template('index.html', error=weather_data.get('error', 'No se encontró información del clima para la ciudad especificada.'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/clima.py
+++ app/clima.py
@@ -1,24 +1,31 @@
+import os
 import requests
 
 def get_weather(city):
-    api_key = "969c2d2ff445f9873d4afa65e8748366"
+    api_key = os.getenv('API_KEY')
     base_url = "http://api.openweathermap.org/data/2.5/weather" 
     complete_url = f"{base_url}?q={city}&lang=es&appid={api_key}&units=metric"
-    response = requests.get(complete_url)
+    
+    try:
+        response = requests.get(complete_url)
+        response.raise_for_status()
 
-    if response.status_code == 200:
-        data = response.json()
-        main = data['main']
-        wind = data['wind']
-        weather_desc = data['weather'][0]['description']    
-        return {
-            "temperature": main['temp'],
-            "pressure": main['pressure'],
-            "humidity": main['humidity'],
-            "wind_speed": wind['speed'],
-            "description": weather_desc
-        }
-    else:
-        return {"error": "City not found"}
+        if response.status_code == 200:
+            data = response.json()
+            main = data['main']
+            wind = data['wind']
+            weather_desc = data['weather'][0]['description']    
+            return {
+                "temperature": main['temp'],
+                "pressure": main['pressure'],
+                "humidity": main['humidity'],
+                "wind_speed": wind['speed'],
+                "description": weather_desc
+            }
+        else:
+            return {"error": "Ciudad no encontrada"}
+    except requests.exceptions.RequestException as e:
+        return {"error": f"Error al conectar con la API: {str(e)}"}
+    except (KeyError, ValueError) as e:
+        return {"error": f"Error al procesar los datos: {str(e)}"}
 
-#print(get_weather("Madrid"))    
